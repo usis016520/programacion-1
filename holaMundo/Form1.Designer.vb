a582aacdd6eb3c9cb5ad2cccc7747d388d492c38
Roberto Carlos Hernandez Melendez <usis016520@ugb.edu.sv>
2020-09-09 14:00:36 -0600

    POO

--- holaMundo/Form1.Designer.vb
+++ holaMundo/Form1.Designer.vb
@@ -22,133 +22,239 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblinteresCompuesto = New System.Windows.Forms.Label()
-        Me.lblInteresSimple = New System.Windows.Forms.Label()
-        Me.txtTiempo = New System.Windows.Forms.TextBox()
-        Me.lbltiempo = New System.Windows.Forms.Label()
-        Me.txtinteres = New System.Windows.Forms.TextBox()
-        Me.lblinteres = New System.Windows.Forms.Label()
-        Me.txtcapital = New System.Windows.Forms.TextBox()
-        Me.lblcapital = New System.Windows.Forms.Label()
-        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.lbltotalx21xf1 = New System.Windows.Forms.Label()
+        Me.lbltotalx1xf1 = New System.Windows.Forms.Label()
+        Me.lbltotalf1 = New System.Windows.Forms.Label()
+        Me.grdEstadistica = New System.Windows.Forms.DataGridView()
+        Me.x1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.f1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.n1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.x1xf1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.x21xf1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.lblRespuestaDesvTipica = New System.Windows.Forms.Label()
+        Me.lbldesvtipica = New System.Windows.Forms.Label()
+        Me.lblVarianza = New System.Windows.Forms.Label()
+        Me.lblMedia = New System.Windows.Forms.Label()
+        Me.lblRespuestaVarianza = New System.Windows.Forms.Label()
+        Me.lblRespuestaMedia = New System.Windows.Forms.Label()
+        Me.lblserie = New System.Windows.Forms.Label()
+        Me.txtserie = New System.Windows.Forms.TextBox()
+        Me.btnMediaAritmetica = New System.Windows.Forms.Button()
+        CType(Me.grdEstadistica, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'lblinteresCompuesto
+        'lbltotalx21xf1
         '
-        Me.lblinteresCompuesto.AutoSize = True
-        Me.lblinteresCompuesto.Location = New System.Drawing.Point(357, 133)
-        Me.lblinteresCompuesto.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblinteresCompuesto.Name = "lblinteresCompuesto"
-        Me.lblinteresCompuesto.Size = New System.Drawing.Size(142, 17)
-        Me.lblinteresCompuesto.TabIndex = 17
-        Me.lblinteresCompuesto.Text = "Interes Compuesto: ?"
+        Me.lbltotalx21xf1.AutoSize = True
+        Me.lbltotalx21xf1.Location = New System.Drawing.Point(1011, 537)
+        Me.lbltotalx21xf1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbltotalx21xf1.Name = "lbltotalx21xf1"
+        Me.lbltotalx21xf1.Size = New System.Drawing.Size(28, 17)
+        Me.lbltotalx21xf1.TabIndex = 25
+        Me.lbltotalx21xf1.Text = "0.0"
         '
-        'lblInteresSimple
+        'lbltotalx1xf1
         '
-        Me.lblInteresSimple.AutoSize = True
-        Me.lblInteresSimple.Location = New System.Drawing.Point(89, 133)
-        Me.lblInteresSimple.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblInteresSimple.Name = "lblInteresSimple"
-        Me.lblInteresSimple.Size = New System.Drawing.Size(113, 17)
-        Me.lblInteresSimple.TabIndex = 16
-        Me.lblInteresSimple.Text = "Interes Simple: ?"
+        Me.lbltotalx1xf1.AutoSize = True
+        Me.lbltotalx1xf1.Location = New System.Drawing.Point(878, 537)
+        Me.lbltotalx1xf1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbltotalx1xf1.Name = "lbltotalx1xf1"
+        Me.lbltotalx1xf1.Size = New System.Drawing.Size(28, 17)
+        Me.lbltotalx1xf1.TabIndex = 24
+        Me.lbltotalx1xf1.Text = "0.0"
         '
-        'txtTiempo
+        'lbltotalf1
         '
-        Me.txtTiempo.Location = New System.Drawing.Point(506, 63)
-        Me.txtTiempo.Margin = New System.Windows.Forms.Padding(4)
-        Me.txtTiempo.Name = "txtTiempo"
-        Me.txtTiempo.Size = New System.Drawing.Size(64, 22)
-        Me.txtTiempo.TabIndex = 15
+        Me.lbltotalf1.AutoSize = True
+        Me.lbltotalf1.Location = New System.Drawing.Point(603, 537)
+        Me.lbltotalf1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbltotalf1.Name = "lbltotalf1"
+        Me.lbltotalf1.Size = New System.Drawing.Size(28, 17)
+        Me.lbltotalf1.TabIndex = 23
+        Me.lbltotalf1.Text = "0.0"
         '
-        'lbltiempo
+        'grdEstadistica
         '
-        Me.lbltiempo.AutoSize = True
-        Me.lbltiempo.Location = New System.Drawing.Point(394, 67)
-        Me.lbltiempo.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lbltiempo.Name = "lbltiempo"
-        Me.lbltiempo.Size = New System.Drawing.Size(105, 17)
-        Me.lbltiempo.TabIndex = 14
-        Me.lbltiempo.Text = "Tiempo (Años):"
+        Me.grdEstadistica.AllowUserToDeleteRows = False
+        Me.grdEstadistica.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdEstadistica.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.x1, Me.f1, Me.n1, Me.x1xf1, Me.x21xf1})
+        Me.grdEstadistica.Location = New System.Drawing.Point(330, 40)
+        Me.grdEstadistica.Margin = New System.Windows.Forms.Padding(4)
+        Me.grdEstadistica.Name = "grdEstadistica"
+        Me.grdEstadistica.RowHeadersWidth = 51
+        Me.grdEstadistica.Size = New System.Drawing.Size(724, 484)
+        Me.grdEstadistica.TabIndex = 22
         '
-        'txtinteres
+        'x1
         '
-        Me.txtinteres.Location = New System.Drawing.Point(310, 63)
-        Me.txtinteres.Margin = New System.Windows.Forms.Padding(4)
-        Me.txtinteres.Name = "txtinteres"
-        Me.txtinteres.Size = New System.Drawing.Size(72, 22)
-        Me.txtinteres.TabIndex = 13
+        Me.x1.HeaderText = "X1"
+        Me.x1.MinimumWidth = 6
+        Me.x1.Name = "x1"
+        Me.x1.Width = 125
         '
-        'lblinteres
+        'f1
         '
-        Me.lblinteres.AutoSize = True
-        Me.lblinteres.Location = New System.Drawing.Point(250, 67)
-        Me.lblinteres.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblinteres.Name = "lblinteres"
-        Me.lblinteres.Size = New System.Drawing.Size(55, 17)
-        Me.lblinteres.TabIndex = 12
-        Me.lblinteres.Text = "Interes:"
+        Me.f1.HeaderText = "F1"
+        Me.f1.MinimumWidth = 6
+        Me.f1.Name = "f1"
+        Me.f1.Width = 125
         '
-        'txtcapital
+        'n1
         '
-        Me.txtcapital.Location = New System.Drawing.Point(108, 63)
-        Me.txtcapital.Margin = New System.Windows.Forms.Padding(4)
-        Me.txtcapital.Name = "txtcapital"
-        Me.txtcapital.Size = New System.Drawing.Size(135, 22)
-        Me.txtcapital.TabIndex = 11
+        Me.n1.HeaderText = "N1"
+        Me.n1.MinimumWidth = 6
+        Me.n1.Name = "n1"
+        Me.n1.Width = 125
         '
-        'lblcapital
+        'x1xf1
         '
-        Me.lblcapital.AutoSize = True
-        Me.lblcapital.Location = New System.Drawing.Point(48, 67)
-        Me.lblcapital.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblcapital.Name = "lblcapital"
-        Me.lblcapital.Size = New System.Drawing.Size(55, 17)
-        Me.lblcapital.TabIndex = 10
-        Me.lblcapital.Text = "Capital:"
+        Me.x1xf1.HeaderText = "X1 x F1"
+        Me.x1xf1.MinimumWidth = 6
+        Me.x1xf1.Name = "x1xf1"
+        Me.x1xf1.Width = 125
         '
-        'btnCalcular
+        'x21xf1
         '
-        Me.btnCalcular.Location = New System.Drawing.Point(205, 201)
-        Me.btnCalcular.Margin = New System.Windows.Forms.Padding(4)
-        Me.btnCalcular.Name = "btnCalcular"
-        Me.btnCalcular.Size = New System.Drawing.Size(197, 58)
-        Me.btnCalcular.TabIndex = 9
-        Me.btnCalcular.Text = "Calcular"
-        Me.btnCalcular.UseVisualStyleBackColor = True
+        Me.x21xf1.HeaderText = "X21 x F1"
+        Me.x21xf1.MinimumWidth = 6
+        Me.x21xf1.Name = "x21xf1"
+        Me.x21xf1.Width = 125
+        '
+        'lblRespuestaDesvTipica
+        '
+        Me.lblRespuestaDesvTipica.AutoSize = True
+        Me.lblRespuestaDesvTipica.Location = New System.Drawing.Point(114, 160)
+        Me.lblRespuestaDesvTipica.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblRespuestaDesvTipica.Name = "lblRespuestaDesvTipica"
+        Me.lblRespuestaDesvTipica.Size = New System.Drawing.Size(16, 17)
+        Me.lblRespuestaDesvTipica.TabIndex = 21
+        Me.lblRespuestaDesvTipica.Text = "?"
+        '
+        'lbldesvtipica
+        '
+        Me.lbldesvtipica.AutoSize = True
+        Me.lbldesvtipica.Location = New System.Drawing.Point(13, 160)
+        Me.lbldesvtipica.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbldesvtipica.Name = "lbldesvtipica"
+        Me.lbldesvtipica.Size = New System.Drawing.Size(90, 17)
+        Me.lbldesvtipica.TabIndex = 20
+        Me.lbldesvtipica.Text = "Desv. Tipica:"
+        '
+        'lblVarianza
+        '
+        Me.lblVarianza.AutoSize = True
+        Me.lblVarianza.Location = New System.Drawing.Point(13, 124)
+        Me.lblVarianza.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblVarianza.Name = "lblVarianza"
+        Me.lblVarianza.Size = New System.Drawing.Size(68, 17)
+        Me.lblVarianza.TabIndex = 19
+        Me.lblVarianza.Text = "Varianza:"
+        '
+        'lblMedia
+        '
+        Me.lblMedia.AutoSize = True
+        Me.lblMedia.Location = New System.Drawing.Point(13, 93)
+        Me.lblMedia.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblMedia.Name = "lblMedia"
+        Me.lblMedia.Size = New System.Drawing.Size(50, 17)
+        Me.lblMedia.TabIndex = 18
+        Me.lblMedia.Text = "Media:"
+        '
+        'lblRespuestaVarianza
+        '
+        Me.lblRespuestaVarianza.AutoSize = True
+        Me.lblRespuestaVarianza.Location = New System.Drawing.Point(114, 124)
+        Me.lblRespuestaVarianza.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblRespuestaVarianza.Name = "lblRespuestaVarianza"
+        Me.lblRespuestaVarianza.Size = New System.Drawing.Size(16, 17)
+        Me.lblRespuestaVarianza.TabIndex = 17
+        Me.lblRespuestaVarianza.Text = "?"
+        '
+        'lblRespuestaMedia
+        '
+        Me.lblRespuestaMedia.AutoSize = True
+        Me.lblRespuestaMedia.Location = New System.Drawing.Point(114, 93)
+        Me.lblRespuestaMedia.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblRespuestaMedia.Name = "lblRespuestaMedia"
+        Me.lblRespuestaMedia.Size = New System.Drawing.Size(16, 17)
+        Me.lblRespuestaMedia.TabIndex = 16
+        Me.lblRespuestaMedia.Text = "?"
+        '
+        'lblserie
+        '
+        Me.lblserie.AutoSize = True
+        Me.lblserie.Location = New System.Drawing.Point(13, 44)
+        Me.lblserie.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblserie.Name = "lblserie"
+        Me.lblserie.Size = New System.Drawing.Size(45, 17)
+        Me.lblserie.TabIndex = 15
+        Me.lblserie.Text = "Serie:"
+        '
+        'txtserie
+        '
+        Me.txtserie.Location = New System.Drawing.Point(66, 40)
+        Me.txtserie.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtserie.Name = "txtserie"
+        Me.txtserie.Size = New System.Drawing.Size(232, 22)
+        Me.txtserie.TabIndex = 14
+        Me.txtserie.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnMediaAritmetica
+        '
+        Me.btnMediaAritmetica.Location = New System.Drawing.Point(17, 519)
+        Me.btnMediaAritmetica.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnMediaAritmetica.Name = "btnMediaAritmetica"
+        Me.btnMediaAritmetica.Size = New System.Drawing.Size(283, 52)
+        Me.btnMediaAritmetica.TabIndex = 13
+        Me.btnMediaAritmetica.Text = "Calculos estadisticos"
+        Me.btnMediaAritmetica.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(619, 322)
-        Me.Controls.Add(Me.lblinteresCompuesto)
-        Me.Controls.Add(Me.lblInteresSimple)
-        Me.Controls.Add(Me.txtTiempo)
-        Me.Controls.Add(Me.lbltiempo)
-        Me.Controls.Add(Me.txtinteres)
-        Me.Controls.Add(Me.lblinteres)
-        Me.Controls.Add(Me.txtcapital)
-        Me.Controls.Add(Me.lblcapital)
-        Me.Controls.Add(Me.btnCalcular)
+        Me.ClientSize = New System.Drawing.Size(1067, 611)
+        Me.Controls.Add(Me.lbltotalx21xf1)
+        Me.Controls.Add(Me.lbltotalx1xf1)
+        Me.Controls.Add(Me.lbltotalf1)
+        Me.Controls.Add(Me.grdEstadistica)
+        Me.Controls.Add(Me.lblRespuestaDesvTipica)
+        Me.Controls.Add(Me.lbldesvtipica)
+        Me.Controls.Add(Me.lblVarianza)
+        Me.Controls.Add(Me.lblMedia)
+        Me.Controls.Add(Me.lblRespuestaVarianza)
+        Me.Controls.Add(Me.lblRespuestaMedia)
+        Me.Controls.Add(Me.lblserie)
+        Me.Controls.Add(Me.txtserie)
+        Me.Controls.Add(Me.btnMediaAritmetica)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Margin = New System.Windows.Forms.Padding(4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.Text = "Calculadora Basica"
+        CType(Me.grdEstadistica, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblinteresCompuesto As Label
-    Friend WithEvents lblInteresSimple As Label
-    Friend WithEvents txtTiempo As TextBox
-    Friend WithEvents lbltiempo As Label
-    Friend WithEvents txtinteres As TextBox
-    Friend WithEvents lblinteres As Label
-    Friend WithEvents txtcapital As TextBox
-    Friend WithEvents lblcapital As Label
-    Friend WithEvents btnCalcular As Button
+    Friend WithEvents lbltotalx21xf1 As Label
+    Friend WithEvents lbltotalx1xf1 As Label
+    Friend WithEvents lbltotalf1 As Label
+    Friend WithEvents grdEstadistica As DataGridView
+    Friend WithEvents x1 As DataGridViewTextBoxColumn
+    Friend WithEvents f1 As DataGridViewTextBoxColumn
+    Friend WithEvents n1 As DataGridViewTextBoxColumn
+    Friend WithEvents x1xf1 As DataGridViewTextBoxColumn
+    Friend WithEvents x21xf1 As DataGridViewTextBoxColumn
+    Friend WithEvents lblRespuestaDesvTipica As Label
+    Friend WithEvents lbldesvtipica As Label
+    Friend WithEvents lblVarianza As Label
+    Friend WithEvents lblMedia As Label
+    Friend WithEvents lblRespuestaVarianza As Label
+    Friend WithEvents lblRespuestaMedia As Label
+    Friend WithEvents lblserie As Label
+    Friend WithEvents txtserie As TextBox
+    Friend WithEvents btnMediaAritmetica As Button
 End Class
--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,61 +1,45 @@
 ﻿Public Class Form1
-    Dim objInteres As New interes()
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
-        objInteres.inter = txtinteres.Text
-        objInteres.tiempo = txtTiempo.Text
-        objInteres.monto = txtcapital.Text
-        lblInteresSimple.Text = "Interes Simple: $" & objInteres.interesSimple()
-        lblinteresCompuesto.Text = "Interes Compuesto: $" & objInteres.interesCompuesto()
+    Dim objEstadistica As New estadistica
+    Private Sub btnMediaAritmetica_Click(sender As Object, e As EventArgs) Handles btnMediaAritmetica.Click
+        lblRespuestaMedia.Text = objEstadistica.calcularMedia(txtserie.Text.Split(","))
+        lblRespuestaVarianza.Text = objEstadistica.calcularVarianza(txtserie.Text.Split(","))
+        lblRespuestaDesvTipica.Text = objEstadistica.calcularDesvTipica(txtserie.Text.Split(","))
+    End Sub
+
+    Private Sub grdEstadistica_KeyUp(sender As Object, e As KeyEventArgs) Handles grdEstadistica.KeyUp
+        Dim nfilas = grdEstadistica.Rows.Count - 1,
+            totalf1 = 0,
+            totalx1xf1 = 0.0,
+            totalx21xf1 = 0.0
+        Dim fila As New DataGridViewRow
+        For i = 0 To nfilas - 1
+            fila = grdEstadistica.Rows(i)
+            Dim x1 = 0, f1 = 0
+            If fila.Cells("x1").Value <> "" Then
+                x1 = Integer.Parse(fila.Cells("x1").Value.ToString())
+            End If
+            If fila.Cells("f1").Value <> "" Then
+                f1 = Integer.Parse(fila.Cells("f1").Value.ToString())
+            End If
+            totalf1 += f1
+            totalx1xf1 += x1 * f1
+            totalx21xf1 += x1 ^ 2 * f1
+
+            fila.Cells("n1").Value = totalf1.ToString()
+            fila.Cells("x1xf1").Value = (x1 * f1).ToString()
+            fila.Cells("x21xf1").Value = (x1 ^ 2 * f1).ToString()
+        Next
+        lbltotalf1.Text = totalf1.ToString()
+        lbltotalx1xf1.Text = totalx1xf1.ToString()
+        lbltotalx21xf1.Text = totalx21xf1.ToString()
+
+        Dim media = Math.Round(totalx1xf1 / totalf1, 2),
+            varianza = Math.Round(totalx21xf1 / totalf1 - media ^ 2, 2)
+        lblRespuestaMedia.Text = media.ToString()
+        lblRespuestaVarianza.Text = varianza.ToString()
+        lblRespuestaDesvTipica.Text = Math.Round(Math.Sqrt(varianza), 2).ToString()
+
     End Sub
 End Class
 
-Class interes
-    Dim _interes As Double, _ntiempo As Int16, _capital As Double
-
-    Public Property inter
-        Set(value)
-            If value >= 0 And value <= 100 Then
-                _interes = value
-            Else
-                MessageBox.Show("El valor del interes no es valido", "Intereses", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            End If
-        End Set
-        Get
-            Return _interes
-        End Get
-    End Property
-    Public Property tiempo
-        Set(value)
-            If value >= 1 And value <= 120 Then
-                _ntiempo = value
-            Else
-                MessageBox.Show("Tiempo no es valido", "Intereses", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            End If
-        End Set
-        Get
-            Return _ntiempo
-        End Get
-    End Property
-    Public Property monto
-        Set(value)
-            If value > 0 Then
-                _capital = value
-            Else
-                MessageBox.Show("El capital debe ser mayor a 0", "Intereses", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            End If
-        End Set
-        Get
-            Return _capital
-        End Get
-    End Property
-
-    Public Function interesSimple()
-        Dim ins = _capital * (_interes / 100) * (_ntiempo / 12)
-        Return Math.Round(ins, 2)
-    End Function
-    Public Function interesCompuesto()
-        Dim inC = _capital * (1 + _interes / 100) ^ _ntiempo
-        Return Math.Round(inC, 2)
-    End Function
-End Class
 
